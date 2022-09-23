d56a0d878a991ddf099c427619b73f1a1a9a0f15
unknown <malekabbes665@gmail.com>
2022-09-23 01:21:56 +0100

    first version :rocket:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentComponent } from './content/content.component';
+import { SlideComponent } from './slide/slide.component';
+import { ListuserComponent } from './listuser/listuser.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    ContentComponent,
+    SlideComponent,
+    ListuserComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent,NavbarComponent,FooterComponent,ContentComponent,SlideComponent]
 })
 export class AppModule { }
